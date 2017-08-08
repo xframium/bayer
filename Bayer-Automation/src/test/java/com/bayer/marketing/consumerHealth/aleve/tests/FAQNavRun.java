2ab60f8b387573b7bbcb9242d62addcf62d85401
GHBBO <GHBBO@WUSPTBC0462.bayer.cnb>
2017-08-08 14:13:44 -0400

    FAQ

--- Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/aleve/tests/FAQNavRun.java
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/aleve/tests/FAQNavRun.java
@@ -3,7 +3,6 @@ package com.bayer.marketing.consumerHealth.aleve.tests;
 import org.testng.annotations.Test;
 import com.bayer.common.Navigate;
 import com.bayer.common.utility.StructureValidator;
-import com.bayer.marketing.consumerHealth.aleve.tests.FAQ.FAQNav;
 import com.bayer.marketing.consumerHealth.aleve.tests.steps.FAQNavStep;
 import com.bayer.test.AbstractTest;
 import com.bayer.test.device.DeviceContainer;
