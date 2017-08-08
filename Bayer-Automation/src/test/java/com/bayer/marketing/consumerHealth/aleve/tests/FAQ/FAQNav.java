da69c8d715cd879890fd9b916db6f13a92557e55
GHBBO <GHBBO@WUSPTBC0462.bayer.cnb>
2017-08-08 14:04:22 -0400

    FAQ Page

---
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/aleve/tests/FAQ/FAQNav.java
@@ -0,0 +1,45 @@
+package com.bayer.marketing.consumerHealth.aleve.tests.FAQ;
+
+import com.bayer.BayerWebDriver;
+import com.bayer.BayerWebElement;
+import com.bayer.test.step.AbstractStep;
+
+public class FAQNav extends AbstractStep
+{
+
+    public FAQNav()
+    {
+        super( "message", "error message" );
+    }
+    
+    @Override
+    protected boolean _executeStep( BayerWebDriver webDriver )
+    {
+        BayerWebElement faq = getElement( "aleve.faq.link.faq", webDriver );
+        faq.click();
+        
+        BayerWebElement ingredients = getElement( "aleve.faq.link.ingredients", webDriver );
+        ingredients.click();
+        
+        BayerWebElement dosage = getElement( "aleve.faq.link.dosage", webDriver );
+        dosage.click();
+        
+        BayerWebElement labelandusage = getElement( "aleve.faq.link.labelandusage", webDriver );
+        labelandusage.click();
+        
+        BayerWebElement packaging = getElement( "aleve.faq.link.packaging", webDriver );
+        packaging.click();
+        
+        BayerWebElement alevepm = getElement( "aleve.faq.link.alevepm", webDriver );
+        alevepm.click();
+        
+        BayerWebElement aleved = getElement( "aleve.faq.link.aleved", webDriver );
+        aleved.click();
+        
+        BayerWebElement sinusandcold = getElement( "aleve.faq.link.sinusandcold", webDriver );
+        sinusandcold.click();
+        
+        return true;
+    }
+
+}
