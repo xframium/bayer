11a6a05b88ed0c9edf12ac5a7809dce300d2e690
Colin <colin.ebneth@bayer.com>
2017-08-15 11:20:29 -0400

    Example Template Folder and Files created

--- Bayer-Automation/src/test/java/com/bayer/exampleTemplate/ExamplePage.java
+++ Bayer-Automation/src/test/java/com/bayer/exampleTemplate/ExamplePage.java
@@ -1,28 +1,28 @@
-package com.bayer.marketing.consumerHealth.betaseron.tests;
+package com.bayer.exampleTemplate;
 
 import org.testng.annotations.Test;
 import com.bayer.common.Navigate;
-import com.bayer.common.TimedNavigate;
 import com.bayer.common.utility.StructureValidator;
 import com.bayer.test.AbstractTest;
 import com.bayer.test.device.DeviceContainer;
 import com.bayer.test.step.factory.Step;
 
-public class HomePage extends AbstractTest
+public class ExamplePage extends AbstractTest
 {
     @TestDescriptor( testName="Navigation Test" )
-    @Test ( dataProvider = "deviceList", enabled=false)
+    @Test ( dataProvider = "deviceList", enabled=true)
     public void navigateTest( DeviceContainer dC )
     {
-        executeSteps( new Step[] { new Navigate( "https://www.betaseron.com/" ) } );
+        executeSteps( new Step[] { new Navigate( "https://www.bayer.com/" ) } );
     }
     
     @TestDescriptor( testName="Structure Test" )
-    @Test ( dataProvider = "deviceList", enabled=true)
+    @Test ( dataProvider = "deviceList", enabled=false)
     public void structureTest( DeviceContainer dC )
     {
-        executeSteps( new Step[] { new TimedNavigate( "https://www.betaseron.com/important-safety-information/", 5000 ), new StructureValidator( "ISI" ) } );
+        executeSteps( new Step[] { new Navigate( "https://www.bayer.com/" ), new StructureValidator( "ISI" ), new Navigate("https://www.aleve.com/live-well/") } );
     }
     
 }
 
+// push test 5
