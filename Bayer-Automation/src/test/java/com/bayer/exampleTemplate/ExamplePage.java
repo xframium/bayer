c96e3f174caceae4f12a100dc2787cfec7229f6e
Colin <colin.ebneth@bayer.com>
2017-08-15 14:13:54 -0400

    Example modifications

--- Bayer-Automation/src/test/java/com/bayer/exampleTemplate/ExamplePage.java
+++ Bayer-Automation/src/test/java/com/bayer/exampleTemplate/ExamplePage.java
@@ -2,25 +2,30 @@ package com.bayer.exampleTemplate;
 
 import org.testng.annotations.Test;
 import com.bayer.common.Navigate;
+import com.bayer.common.TimedNavigate;
 import com.bayer.common.utility.StructureValidator;
+import com.bayer.marketing.consumerHealth.aleve.tests.steps.FAQNavStep;
 import com.bayer.test.AbstractTest;
 import com.bayer.test.device.DeviceContainer;
 import com.bayer.test.step.factory.Step;
 
-public class ExamplePage extends AbstractTest
-{
+public class ExamplePage extends AbstractTest {
+	
     @TestDescriptor( testName="Navigation Test" )
     @Test ( dataProvider = "deviceList", enabled=true)
-    public void navigateTest( DeviceContainer dC )
-    {
-        executeSteps( new Step[] { new Navigate( "https://www.bayer.com/" ) } );
+    public void navigateTest( DeviceContainer dC ) {
+        executeSteps( new Step[] { new Navigate( "https://www.bayer.com/" ), new ExampleStep() } );
+    }
+    @TestDescriptor( testName="Timed Navigation Test" )
+    @Test ( dataProvider = "deviceList", enabled=true)
+    public void timedNavigateTest( DeviceContainer dC ) {
+        executeSteps( new Step[] { new TimedNavigate( "https://www.bayer.com/", 3000) } );
     }
     
     @TestDescriptor( testName="Structure Test" )
     @Test ( dataProvider = "deviceList", enabled=false)
-    public void structureTest( DeviceContainer dC )
-    {
-        executeSteps( new Step[] { new Navigate( "https://www.bayer.com/" ), new StructureValidator( "ISI" ), new Navigate("https://www.aleve.com/live-well/") } );
+    public void structureTest( DeviceContainer dC ) {
+        executeSteps( new Step[] { new Navigate( "https://www.bayer.com/" ), new StructureValidator( "ISI" ) } );
     }
     
 }
