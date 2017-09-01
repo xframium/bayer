faa81aa15cc05d24969dc2fee6e68083f103b925
Colin <colin.ebneth@bayer.com>
2017-09-01 09:32:40 -0400

    Constructed Claritin Coupon Page

---
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/claritin/tests/Coupons/CouponsNav.java
@@ -0,0 +1,24 @@
+package com.bayer.marketing.consumerHealth.claritin.tests.Coupons;
+
+import org.testng.annotations.Test;
+import com.bayer.common.Navigate;
+import com.bayer.marketing.consumerHealth.aleve.tests.Save.steps.saveStep;
+import com.bayer.marketing.consumerHealth.claritin.tests.Coupons.steps.couponsStep;
+import com.bayer.test.AbstractTest;
+import com.bayer.test.device.DeviceContainer;
+import com.bayer.test.step.factory.Step;
+import org.openqa.selenium.internal.WrapsElement;
+
+public class CouponsNav extends AbstractTest
+{
+   
+   
+    @TestDescriptor( testName="Claritin Coupon Test" )
+    @Test ( dataProvider = "deviceList", enabled=true)
+    public void couponTest( DeviceContainer dC )
+    {
+        executeSteps( new Step[] { new Navigate("https://www.claritin.com/coupons/"), new couponsStep() } );
+    }
+    
+}
+
---
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/claritin/tests/Coupons/steps/couponsStep.java
@@ -0,0 +1,48 @@
+package com.bayer.marketing.consumerHealth.claritin.tests.Coupons.steps;
+
+
+import com.bayer.BayerWebDriver;
+import com.bayer.BayerWebElement;
+import com.bayer.test.step.AbstractStep;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.collections.functors.SwitchTransformer;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.SendKeysAction;
+import org.openqa.selenium.internal.WrapsElement;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class couponsStep extends AbstractStep
+{
+
+    public couponsStep()
+    {
+        super( "message", "error message" );
+    }
+    
+    
+    @Override
+    protected boolean _executeStep( BayerWebDriver webDriver )
+    {
+    	waitForElement( "aleve.coupon.iframe", webDriver, 15 );
+        WebElement iframe = getElement( "aleve.coupon.iframe", webDriver );
+        webDriver.switchTo().frame(0);
+
+    	
+    	waitForElement( "aleve.coupon.aleveDCheck", webDriver, 15 );
+        BayerWebElement aleveDCheck = getElement( "aleve.coupon.aleveDCheck", webDriver );
+        aleveDCheck.click();
+   
+        BayerWebElement print = getElement( "aleve.coupon.print", webDriver );
+       // print.click();
+         
+       
+        
+        return true;
+    }
+
+}
