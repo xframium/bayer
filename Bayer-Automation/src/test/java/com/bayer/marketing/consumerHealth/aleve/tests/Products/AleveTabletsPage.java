e9805c6b3d09dfd9e0f0b3fa2ed8be6d34c8e9dd
Colin <colin.ebneth.ext@bayer.com>
2017-09-08 14:23:29 -0400

    Added Master Test File

--- Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/aleve/tests/Products/AleveTabletsPage.java
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/aleve/tests/Products/AleveTabletsPage.java
@@ -2,6 +2,7 @@ package com.bayer.marketing.consumerHealth.aleve.tests.Products;
 
 import org.testng.annotations.Test;
 import com.bayer.common.Navigate;
+import com.bayer.common.TimedNavigate;
 import com.bayer.common.utility.StructureValidator;
 import com.bayer.test.AbstractTest;
 import com.bayer.test.device.DeviceContainer;
@@ -13,7 +14,7 @@ public class AleveTabletsPage extends AbstractTest
     @Test ( dataProvider = "deviceList", enabled=true)
     public void navigateTest( DeviceContainer dC )
     {
-        executeSteps( new Step[] { new Navigate( "https://www.aleve.com/aleve-tablets/" ) } );
+        executeSteps( new Step[] { new TimedNavigate( "https://www.aleve.com/aleve-tablets/", 3000 ) } );
     }
     
     
