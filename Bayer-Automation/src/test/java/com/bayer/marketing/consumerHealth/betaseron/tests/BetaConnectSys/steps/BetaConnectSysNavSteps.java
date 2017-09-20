e3d26ba913e202a04333a67ebd2c0648eea9aed1
miovi1 <gordon.schenck@bayer.com>
2017-09-20 15:09:17 -0500

    modified to begin handling browser popup blocker

--- Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/BetaConnectSys/steps/BetaConnectSysNavSteps.java
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/BetaConnectSys/steps/BetaConnectSysNavSteps.java
@@ -1,8 +1,11 @@
 package com.bayer.marketing.consumerHealth.betaseron.tests.BetaConnectSys.steps;
 
+import org.openqa.selenium.WebElement;
+
 import com.bayer.BayerWebDriver;
 import com.bayer.BayerWebElement;
 import com.bayer.test.step.AbstractStep;
+import com.gargoylesoftware.htmlunit.WebClient;
 
 public class BetaConnectSysNavSteps extends AbstractStep
 {
@@ -19,7 +22,19 @@ public class BetaConnectSysNavSteps extends AbstractStep
     	BayerWebElement menu = getElement( "betaseron.mobile.menu", webDriver );
 
 	if(menu.isDisplayed()){
-        	
+		
+		WebElement w1 = webDriver.findElementByXPath ( "//*[@resource-id='com.android.chrome:id/menu_button']");
+		w1.click();
+		
+		WebElement w2 = webDriver.findElementByXPath( "//*[@content-desc='Settings']");
+        w2.click();
+        
+		WebElement w3 = webDriver.findElementByXPath( "//*[@text='Site settings']");
+        w3.click();
+        
+		WebElement w4 = webDriver.findElementByXPath( "//*[@text='Pop-ups']");
+        w4.click();
+        
         	menu.click();        	
 
         	BayerWebElement tab3caret = getElement( "betaseron.mobile.menu.tab3.caret", webDriver );
