273553443105ecd612f19f20ab23310c3e5a579e
miovi1 <gordon.schenck@bayer.com>
2017-09-21 15:09:41 -0500

    update mobile menu test for tab 1 dropdown

--- Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/MobileMenu/steps/MobileMenuSteps.java
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/MobileMenu/steps/MobileMenuSteps.java
@@ -45,7 +45,29 @@ public class MobileMenuSteps extends AbstractStep
     	menu.click();
     	waitForElement( "betaseron.mobile.menu.tab5", webDriver, 15 );
     	BayerWebElement tab5 = getElement("betaseron.mobile.menu.tab5", webDriver );
-        tab5.click();           
+        tab5.click(); 
+        
+        menu.click(); 
+        waitForElement( "betaseron.mobile.menu.tab1.caret", webDriver, 15 );
+    	BayerWebElement tab1caret = getElement("betaseron.mobile.menu.tab1", webDriver );
+        tab1caret.click();
+        waitForElement( "betaseron.mobile.menu.tab1.caret.drop1", webDriver, 15 );
+    	BayerWebElement tab1drop1 = getElement("betaseron.mobile.menu.tab1.caret.drop1", webDriver );
+    	tab1drop1.click();
+
+        menu.click(); 
+        waitForElement( "betaseron.mobile.menu.tab1.caret", webDriver, 15 );
+        tab1caret.click();
+        waitForElement( "betaseron.mobile.menu.tab1.caret.drop2", webDriver, 15 );
+    	BayerWebElement tab1drop2 = getElement("betaseron.mobile.menu.tab1.caret.tab2", webDriver );
+    	tab1drop2.click();
+
+        menu.click(); 
+        waitForElement( "betaseron.mobile.menu.tab1.caret", webDriver, 15 );
+    	tab1caret.click();
+        waitForElement( "betaseron.mobile.menu.tab1.caret.drop3", webDriver, 15 );
+    	BayerWebElement tab1drop3 = getElement("betaseron.mobile.menu.tab1.caret.tab3", webDriver );
+    	tab1drop3.click();        
 	}       
          
         return true;
