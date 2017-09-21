dbd7b7738b3b3eeb94f95ed2adfc5ba5622fc395
miovi1 <gordon.schenck@bayer.com>
2017-09-21 14:05:21 -0500

    update executeSteps method

--- Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/MobileMenu/MobileMenu.java
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/MobileMenu/MobileMenu.java
@@ -3,6 +3,7 @@ package com.bayer.marketing.consumerHealth.betaseron.tests.MobileMenu;
 import org.testng.annotations.Test;
 import com.bayer.common.Navigate;
 import com.bayer.marketing.consumerHealth.betaseron.tests.BetaConnectSys.steps.BetaConnectSysNavSteps;
+import com.bayer.marketing.consumerHealth.betaseron.tests.MobileMenu.steps.MobileMenuSteps;
 import com.bayer.test.AbstractTest;
 import com.bayer.test.device.DeviceContainer;
 import com.bayer.test.step.factory.Step;
@@ -10,7 +11,7 @@ import com.bayer.test.step.factory.Step;
 public class MobileMenu extends AbstractTest
 {
    
-    @TestDescriptor( testName="MobileMenu Test" )
+    @TestDescriptor( testName="Mobile Menu Test" )
     @Test ( dataProvider = "deviceList", enabled=true)
     public void MobileMenuTest( DeviceContainer dC )
     {
