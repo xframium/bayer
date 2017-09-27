26c07945e19da2466f53939ca8dafa44f3877c05
EMNJP <michael.ohandley.ext@bayer.com>
2017-09-27 17:00:50 -0400

    Taking Betaseron updates

--- Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/TakingBetaseron/TakingBetaseron.java
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/TakingBetaseron/TakingBetaseron.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import com.bayer.common.Navigate;
 import com.bayer.common.TimedNavigate;
 import com.bayer.common.utility.StructureValidator;
+import com.bayer.marketing.consumerHealth.betaseron.tests.TakingBetaseron.steps.TakingBetaseronSteps;
 import com.bayer.test.AbstractTest;
 import com.bayer.test.device.DeviceContainer;
 import com.bayer.test.step.factory.Step;
@@ -16,8 +17,8 @@ public class TakingBetaseron extends AbstractTest
     @Test ( dataProvider = "deviceList", enabled=true)
     public void structureTest( DeviceContainer dC )
     {
-        executeSteps( new Step[] { new Navigate( "https://www.betaseron.com/taking-betaseron/"), 
-        						  new StructureValidator( "ISI" ), 
+        executeSteps( new Step[] { new Navigate( "https://www.betaseron.com/taking-betaseron/"), new TakingBetaseronSteps()
+        						  //new StructureValidator( "ISI" ), 
         						 // new StructureValidator("Text Verification") } );
     
         } );
--- Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/TakingBetaseron/steps/TakingBetaseronSteps.java
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/betaseron/tests/TakingBetaseron/steps/TakingBetaseronSteps.java
@@ -1,5 +1,9 @@
 package com.bayer.marketing.consumerHealth.betaseron.tests.TakingBetaseron.steps;
 
+import javax.print.CancelablePrintJob;
+
+import org.eclipse.jetty.client.ContinueProtocolHandler;
+
 import com.bayer.BayerWebDriver;
 import com.bayer.BayerWebElement;
 import com.bayer.test.step.AbstractStep;
@@ -16,15 +20,32 @@ public class TakingBetaseronSteps extends AbstractStep
     protected boolean _executeStep( BayerWebDriver webDriver )
     {
     	waitForElement( "betaseron.home.betaAppVid", webDriver, 15 );
-    	BayerWebElement arthritis = getElement( "aleve.understand.arthritis", webDriver );
-    	arthritis.click();
+    	BayerWebElement vid = getElement( "betaseron.home.betaAppVid", webDriver );
+    	vid.click();
     	
-    	waitForElement( "aleve.test", webDriver, 15 );
-    	webDriver.navigate().to("https://www.betaseron.com/");
-       
+    	waitForElement( "betaseron.home.betaAppVid", webDriver, 15 );
+    	BayerWebElement vid = getElement( "betaseron.home.betaAppVid", webDriver );
+    	vid.click();
     	
+    	
+    	waitForElement( "betaseron.taking.appleApp", webDriver, 15 );
+    	BayerWebElement apple = getElement( "betaseron.taking.appleApp", webDriver );
+    	apple.click();
+    	
+    	waitForElement( "betaseron.taking.cancel", webDriver, 15 );
+    	BayerWebElement cancel = getElement( "betaseron.taking.cancel", webDriver );
+    	cancel.click();
+   
+    	
+    	waitForElement( "betaseron.taking.androidApp", webDriver, 15 );
+    	BayerWebElement android = getElement( "betaseron.taking.androidApp", webDriver );
+    	android.click();
+    	
+    	waitForElement( "betaseron.taking.continue", webDriver, 15 );
+    	BayerWebElement continueButton = getElement( "betaseron.taking.continue", webDriver );
+        continueButton.click(); 
         
-         
+        
         
         return true;
     }
