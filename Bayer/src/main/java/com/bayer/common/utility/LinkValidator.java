0d53d7badd708696c0b08cddf7312a3aa29a9190
Allen Geary <ageary@morelandlabs.com>
2017-09-28 10:31:41 -0400

    Link check

---
+++ Bayer/src/main/java/com/bayer/common/utility/LinkValidator.java
@@ -0,0 +1,43 @@
+package com.bayer.common.utility;
+
+import static org.testng.Assert.assertTrue;
+import java.net.URL;
+import com.bayer.BayerWebDriver;
+import com.bayer.test.step.AbstractStep;
+import com.bayer.utiilty.HTTPLinkCheck;
+
+public class LinkValidator extends AbstractStep
+{
+    private String initialUrl;
+    private int pageCount;
+    
+    public LinkValidator( String initialUrl, int pageCount )
+    {
+        super( "Generic Link Validator", "Generic Link Validator" );
+    }
+
+    
+
+    @Override
+    protected boolean _executeStep( BayerWebDriver webDriver )
+    {
+        HTTPLinkCheck linkCheck = new HTTPLinkCheck();
+        try
+        {
+            linkCheck.process( new URL( initialUrl ), new URL( initialUrl ) );
+            
+            assertTrue( pageCount == linkCheck.getLinkMap().size() );
+            assertTrue( linkCheck.getBrokenLinks().isEmpty() );
+            
+        }
+        catch( Exception e )
+        {
+            log.error( "Error processing URL", e );
+            return false;
+        }
+
+        
+        return true;
+    }
+
+}
