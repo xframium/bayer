bfb573e7ed4db9a31a73d1b2e12b2764a2cc90fd
Laxmansingh Muriyal <lakshman.muriyal@gmail.com>
2017-12-19 21:52:05 +0530

    Aspirin Site Map module automation scripts.

--- Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/aspirin/tests/siteMap/steps/SiteMapStrokeNavStep.java
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/aspirin/tests/siteMap/steps/SiteMapStrokeNavStep.java
@@ -28,6 +28,62 @@ public class SiteMapStrokeNavStep extends AbstractStep {
 		this.validate("aspirin.sitemap.what.is.stroke", webDriver, "aspirin.sitemap.what.is.stroke.validate", wait);
 		Util.recallBaseURL(webDriver, siteMap);
 		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.how.stroke.affect.body", webDriver, "aspirin.sitemap.how.stroke.affect.body.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.warning.signs", webDriver, "aspirin.sitemap.warning.signs.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.types.of.stroke", webDriver, "aspirin.sitemap.types.of.stroke.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.stroke.risk.factor", webDriver, "aspirin.sitemap.stroke.risk.factor.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.stroke.risk.factor.12", webDriver, "aspirin.sitemap.stroke.risk.factor.12.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.stroke.risk.factor.women", webDriver, "aspirin.sitemap.stroke.risk.factor.women.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.stroke.prevention.clot.related", webDriver, "aspirin.sitemap.stroke.prevention.clot.related.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.prevention.clot.related.steps", webDriver, "aspirin.sitemap.prevention.clot.related.steps.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.prevention.clot.how.aspirin.works", webDriver, "aspirin.sitemap.prevention.clot.how.aspirin.works.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.life.after.stroke", webDriver, "aspirin.sitemap.life.after.stroke.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.stroke.rehabilitation.exercises", webDriver, "aspirin.sitemap.stroke.rehabilitation.exercises.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.stroke.recovery.exercises", webDriver, "aspirin.sitemap.stroke.recovery.exercises.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.stroke.caregiver.information", webDriver, "aspirin.sitemap.stroke.caregiver.information.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
+		
+		this.validate("aspirin.sitemap.together.end.stroke", webDriver, "aspirin.sitemap.together.end.stroke.validate", wait);
+		Util.recallBaseURL(webDriver, siteMap);
+		waitForElement("aspirin.sitemap.base.test", webDriver, 15);
 				
 			
 		return true;
