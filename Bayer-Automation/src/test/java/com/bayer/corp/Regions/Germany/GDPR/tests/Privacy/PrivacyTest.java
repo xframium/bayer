1b8acaf26c3bd6c5f0696109332024c51eaed5ca
Colin <colin.ebneth.ext@bayer.com>
2018-01-22 17:21:47 -0500

    Update GDPR privacy test

--- Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyTest.java
+++ Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyTest.java
@@ -4,9 +4,16 @@ import com.bayer.exampleTemplate.tests.Example.steps.ExampleStep;
 import com.bayer.corp.Regions.Germany.GDPR.tests.Privacy.steps.PrivacyPolicy;
 import com.bayer.exampleTemplate.tests.Example.steps.ConditionsOfUse;
 import org.testng.annotations.Test;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.*;
+import com.bayer.common.Accessibility;
 import com.bayer.common.Navigate;
 import com.bayer.common.TimedNavigate;
 import com.bayer.test.AbstractTest;
+import com.bayer.test.AbstractTest.TestDescriptor;
 import com.bayer.test.device.DeviceContainer;
 import com.bayer.test.step.factory.Step;
 import org.springframework.ejb.access.SimpleRemoteStatelessSessionProxyFactoryBean;
@@ -15,7 +22,6 @@ import com.bayer.common.utility.StructureValidator;
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.javascript.host.URL;
 import com.gargoylesoftware.htmlunit.javascript.host.dom.Text;
-import java.io.*;
 
 public class PrivacyTest extends AbstractTest {
 		
@@ -25,7 +31,52 @@ public class PrivacyTest extends AbstractTest {
 			public String getUrl(){ 
 				return url;
 			}
-		    
+		    public static void  setURLs() {
+		    	String FILE_NAME = "/tmp/MyFirstExcel.xlsx";
+
+		        
+
+		            XSSFWorkbook workbook = new XSSFWorkbook();
+		            XSSFSheet sheet = workbook.createSheet("Datatypes in Java");
+		            Object[][] datatypes = {
+		                    {"Datatype", "Type", "Size(in bytes)"},
+		                    {"int", "Primitive", 2},
+		                    {"float", "Primitive", 4},
+		                    {"double", "Primitive", 8},
+		                    {"char", "Primitive", 1},
+		                    {"String", "Non-Primitive", "No fixed size"}
+		            };
+
+		            int rowNum = 0;
+		            System.out.println("Creating excel");
+
+		            for (Object[] datatype : datatypes) {
+		                Row row = sheet.createRow(rowNum++);
+		                int colNum = 0;
+		                for (Object field : datatype) {
+		                    Cell cell = row.createCell(colNum++);
+		                    if (field instanceof String) {
+		                        cell.setCellValue((String) field);
+		                    } else if (field instanceof Integer) {
+		                        cell.setCellValue((Integer) field);
+		                    }
+		                }
+		            }
+
+		            try {
+		                FileOutputStream outputStream = new FileOutputStream(FILE_NAME);
+		                workbook.write(outputStream);
+		                workbook.close();
+		            } catch (FileNotFoundException e) {
+		                e.printStackTrace();
+		            } catch (IOException e) {
+		                e.printStackTrace();
+		            }
+
+		            System.out.println("Done");
+		        
+		    }
+
 		    @TestDescriptor( testName="GDPR Privacy Policy Validation" )
 		    @Test ( dataProvider = "deviceList", enabled=true)
 		    public void privacyPolicyTest( DeviceContainer dC ) {
@@ -38,6 +89,12 @@ public class PrivacyTest extends AbstractTest {
 		        executeSteps( new Step[] { new Navigate(url), new ExampleStep() } );
 		    }
 		    
+		    @TestDescriptor ( testName = "GDPR Link Checker Test")
+		    @Test ( dataProvider = "deviceList", enabled = true)
+		    public void gdprLinkChecker( DeviceContainer dC )
+		    {
+		        executeSteps( new Step[] { new Navigate(url), new LinkValidator(url, -1) } );
+		    }
 		    @TestDescriptor( testName="GDPR Timed Navigation Test" )
 		    @Test ( dataProvider = "deviceList", enabled=false)
 		    public void timedNavigateTest( DeviceContainer dC ) {
--- Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/steps/PrivacyPolicy.java
+++ Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/steps/PrivacyPolicy.java
@@ -3,6 +3,10 @@ package com.bayer.corp.Regions.Germany.GDPR.tests.Privacy.steps;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.plaf.basic.BasicSliderUI.ScrollListener;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 import com.bayer.BayerWebDriver;
 import com.bayer.BayerWebElement;
@@ -40,30 +44,72 @@ public class PrivacyPolicy extends AbstractStep
     	WebDriverWait wait = new WebDriverWait(webDriver, 20);
         
     	waitForElement( "bayer.key", webDriver, 15 );
+    	BayerWebElement logo = getElement("bayer.key", webDriver);
     	
         if(webDriver.getPageSource().contains("Datenschutzerklärung")) {
-        	waitForElement( "gdpr.privacyV1", webDriver, 15 );
-            BayerWebElement privacy1 = getElement("gdpr.privacyV1", webDriver);
-            privacy1.click();
+           waitForElement( "gdpr.privacyV1", webDriver, 15 );
+           BayerWebElement privacy1 = getElement("gdpr.privacyV1", webDriver);
+           privacy1.click();
+           waitForElement( "bayer.key", webDriver, 15 );
+           webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+           //logo.click();
+            //System.out.println("Passed if 1");
         	//verify 5 elements on privacy page
-            new StructureValidator("Nutzung der Website", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
-            new StructureValidator("Informationen über Ihre Rechte", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
-            new StructureValidator("Profiling", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
-            new StructureValidator("Datenübertragbarkeit", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
-            new StructureValidator("Aufsichtsbehörde", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
+           if(webDriver.getPageSource().contains("Nutzung der Website")) {
+        	   System.out.println("Test Successful");
+           }
+           else { 
+        	   System.out.println("Text not found");
+           }
+           
+           if(webDriver.getPageSource().contains("Informationen über Ihre Rechte")) {
+        	   System.out.println("Test Successful");
+           }
+           else { 
+        	   System.out.println("Text not found");
+           }
+           
+           if(webDriver.getPageSource().contains("Profiling")) {
+        	   System.out.println("Test Successful");
+           }
+           else { 
+        	   System.out.println("Text not found");
+           }
+           
+           if(webDriver.getPageSource().contains("Datenübertragbarkeit")) {
+        	   System.out.println("Test Successful");
+           }
+           else { 
+        	   System.out.println("Text not found");
+           }
+           
+           if(webDriver.getPageSource().contains("Aufsichtsbehörde")) {
+        	   System.out.println("Test Successful");
+           }
+           else { 
+        	   System.out.println("Text not found");
+           }
+            //new StructureValidator("Nutzung der Website", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
+            //new StructureValidator("Informationen über Ihre Rechte", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
+            //new StructureValidator("Profiling", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
+            //new StructureValidator("Datenübertragbarkeit", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
+            //new StructureValidator("Aufsichtsbehörde", "/Bayer-Automation/src/test/java/com/bayer/corp/Regions/Germany/GDPR/tests/Privacy/PrivacyKeywordValidation.xml");
             
             
             
         }
         else if(webDriver.getPageSource().contains("Datenschutzinformation")) {
+        	System.out.println("Passed if 2");
         	//click privacy
         	//verify 5 elements on privacy page
         }
         else if(webDriver.getPageSource().contains("Datenschutz")) {
+        	System.out.println("Passed if 3");
         	//click privacy
         	//verify 5 elements on privacy page
         }
         else if(webDriver.getPageSource().contains("Datenschutzpolicy")) {
+        	System.out.println("Passed if 4");
         	//click privacy
         	//verify 5 elements on privacy page
         }	
