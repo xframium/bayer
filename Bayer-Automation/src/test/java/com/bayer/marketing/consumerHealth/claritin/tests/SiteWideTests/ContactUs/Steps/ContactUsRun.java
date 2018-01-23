95652117560d98290838133508e3120ec8717cf9
EMNJP <michael.ohandley.ext@bayer.com>
2018-01-23 15:51:51 -0500

    Claritin Contact Us

---
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/claritin/tests/SiteWideTests/ContactUs/Steps/ContactUsRun.java
@@ -0,0 +1,57 @@
+package com.bayer.marketing.consumerHealth.claritin.tests.SiteWideTests.ContactUs.Steps;
+
+import java.io.BufferedReader;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import org.apache.commons.collections.functors.IfClosure;
+
+import java.io.FileReader;
+import java.io.IOException;
+import com.bayer.common.Accessibility;
+import com.bayer.common.utility.*;
+import com.bayer.exampleTemplate.tests.Example.ExamplePage;
+import com.bayer.BayerWebDriver;
+import com.bayer.BayerWebElement;
+import com.bayer.test.step.AbstractStep;
+import com.sun.jna.platform.unix.X11;
+
+public class ContactUsRun extends AbstractStep
+{
+
+    public ContactUsRun()
+    {
+        super( "message", "error message" );
+    }
+    
+    @Override
+    protected boolean _executeStep( BayerWebDriver webDriver ) {	
+    	
+    	
+    	
+    	waitForElement("claritin.contactUs", webDriver, 15);
+    	BayerWebElement contactUs = getElement( "claritin.contactUs", webDriver );
+    	contactUs.click();
+    	
+    	waitForElement("claritin.contactUs.continue", webDriver, 15);
+    	BayerWebElement continueButton = getElement( "claritin.contactUs.continue", webDriver );
+    	continueButton.click();
+    	
+    	String livewellURL ="https://www.livewell.bayer.com/contactus/";
+    	String url = webDriver.getCurrentUrl();
+    	
+    	System.out.println(url);
+    	
+    	if(url.equals(livewellURL)){
+    	System.out.println("Success");	
+    	}else{
+    		throw new IllegalArgumentException();
+    	}
+    	
+    	
+        return true;
+        
+    }
+
+}
