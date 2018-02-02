343c912774f46add9cbd687a203405cb38fefa9d
EMNJP <michael.ohandley.ext@bayer.com>
2018-02-02 11:17:51 -0500

    Contact Us

--- Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/claritin/tests/SiteWideTests/ContactUs/ContactUs.java
+++ Bayer-Automation/src/test/java/com/bayer/marketing/consumerHealth/claritin/tests/SiteWideTests/ContactUs/ContactUs.java
@@ -1,11 +1,12 @@
 package com.bayer.marketing.consumerHealth.claritin.tests.SiteWideTests.ContactUs;
 
-import com.bayer.marketing.consumerHealth.claritin.BlueSkyLiving.tests.HomePage.steps.HomeStep;
+
+import com.bayer.marketing.consumerHealth.claritin.tests.SiteWideTests.ContactUs.Steps.ContactUsRun;
 import com.bayer.exampleTemplate.tests.Example.steps.PrivacyPolicy;
 import com.bayer.exampleTemplate.tests.Example.steps.ConditionsOfUse;
 import org.testng.annotations.Test;
 
-import com.bayer.common.Accessibility;
+//import com.bayer.common.Accessibility;
 import com.bayer.common.Navigate;
 import com.bayer.common.TimedNavigate;
 import com.bayer.test.AbstractTest;
@@ -26,30 +27,31 @@ public String url = "colin:Bayer123@03342-claritin-qa.photoninfotech.com:8081";
 	public String getUrl(){ 
 		return url;
 	}
+	
     @TestDescriptor( testName="Contact Us" )
     @Test ( dataProvider = "deviceList", enabled=true)
     public void conditionsTest( DeviceContainer dC ) {
-        executeSteps( new Step[] { new Navigate(url), new ConditionsOfUse() } );
+        executeSteps( new Step[] { new Navigate(url), new ContactUsRun() } );
     }
-    
+    /*
     @TestDescriptor( testName="BlueSkyLiving Navigation Test" )
     @Test ( dataProvider = "deviceList", enabled=false)
     public void navigateTest( DeviceContainer dC ) {
         executeSteps( new Step[] { new Navigate(url) } );
     }
     @TestDescriptor ( testName = "Claritin Blue Sky General Testing ")
-    @Test ( dataProvider = "deviceList", enabled = true)
+    @Test ( dataProvider = "deviceList", enabled = false)
     public void claristinBSLTest( DeviceContainer dC )
     {
         String baseUrl = "http://colin:Bayer123@03342-claritin-qa.photoninfotech.com:8081/";
-        executeSteps( new Step[] { new Navigate( baseUrl ), new LinkValidator( baseUrl, -1 ), new Accessibility( baseUrl ) } );
+     //   executeSteps( new Step[] { new Navigate( baseUrl ), new LinkValidator( baseUrl, -1 ), new Accessibility( baseUrl ) } );
     }
     @TestDescriptor( testName="BlueSkyLiving Timed Navigation Test" )
     @Test ( dataProvider = "deviceList", enabled=false)
     public void timedNavigateTest( DeviceContainer dC ) {
         executeSteps( new Step[] { new TimedNavigate(url, 6000) } );
     }
-     
+     */
 }
 
 // push test 5
