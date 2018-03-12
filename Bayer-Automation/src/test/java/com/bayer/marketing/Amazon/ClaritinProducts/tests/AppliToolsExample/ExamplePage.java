1a02a6ec7029d5436e2a3a852a97645f4bad6c7d
Colin <colin.ebneth@bayer.com>
2018-03-12 10:54:50 -0400

    Amazon test structure

---
+++ Bayer-Automation/src/test/java/com/bayer/marketing/Amazon/ClaritinProducts/tests/AppliToolsExample/ExamplePage.java
@@ -0,0 +1,81 @@
+package com.bayer.exampleTemplate.tests.AppliToolsExample;
+
+import com.bayer.exampleTemplate.tests.AppliToolsExample.steps.ArchPatientStep;
+import com.bayer.exampleTemplate.tests.AppliToolsExample.steps.ExampleStep;
+import com.bayer.exampleTemplate.tests.Example.steps.PrivacyPolicy;
+import com.bayer.exampleTemplate.tests.Example.steps.ConditionsOfUse;
+import org.testng.annotations.Test;
+import com.bayer.common.Navigate;
+import com.bayer.common.TimedNavigate;
+import com.bayer.test.AbstractTest;
+import com.bayer.test.device.DeviceContainer;
+import com.bayer.test.step.factory.Step;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.springframework.ejb.access.SimpleRemoteStatelessSessionProxyFactoryBean;
+import com.bayer.common.utility.LinkValidator;
+import com.bayer.common.utility.StructureValidator;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.javascript.host.URL;
+import com.gargoylesoftware.htmlunit.javascript.host.dom.Text;
+
+public class ExamplePage extends AbstractTest {
+	
+private String url = "http://test.archpatientassistance.com/";
+private int parentRowNum = 1;  
+	public String getUrl(){ 
+		return url;
+	}
+	
+    @TestDescriptor( testName="AppliTools Example Navigation Test" )
+    @Test ( dataProvider = "deviceList", enabled=false)
+    public void appliToolsNavigateTest( DeviceContainer dC ) {
+    	String [] urlNames = new String[100];
+		int numEntries = 0;
+		try {
+			File file = new File("src/test/java/com/bayer/exampleTemplate/config/TestData/urlList.txt");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			int i = 0;
+			while ((line = bufferedReader.readLine()) != null) {
+				urlNames[i] = line;
+				numEntries++;
+				i++; }
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("For loop");
+		
+		for(int c = 0; c < numEntries; c ++) { 
+			url = urlNames[c];
+			executeSteps( new Step[] { new Navigate(url), new ArchPatientStep(url, parentRowNum)});
+			parentRowNum++;
+			System.out.println("Parent row num is " + parentRowNum);
+		}
+    }
+    @TestDescriptor( testName="Applitools Single Page Example Test" )
+    @Test ( dataProvider = "deviceList", enabled=true)
+    public void appliToolsExample( DeviceContainer dC ) {
+        executeSteps( new Step[] { new Navigate(url), new ArchPatientStep(url, parentRowNum)} );
+    }
+    @TestDescriptor( testName="Applitools Single Page Example Test" )
+    @Test ( dataProvider = "deviceList", enabled=true)
+    public void navTest( DeviceContainer dC ) {
+        executeSteps( new Step[] { new Navigate(url)} );
+    }
+    @TestDescriptor( testName="Example Timed Navigation Test" )
+    @Test ( dataProvider = "deviceList", enabled=false)
+    public void timedNavigateTest( DeviceContainer dC ) {
+        executeSteps( new Step[] { new TimedNavigate(url, 6000) } );
+    }
+     
+}
+
+// push test 5
---
+++ Bayer-Automation/src/test/java/com/bayer/marketing/Amazon/ClaritinProducts/tests/Example/ExamplePage.java
@@ -0,0 +1,41 @@
+package com.bayer.marketing.Amazon.ClaritinProducts.tests.Example;
+
+import com.bayer.exampleTemplate.tests.Example.steps.ExampleStep;
+import com.bayer.exampleTemplate.tests.Example.steps.PrivacyPolicy;
+import com.bayer.exampleTemplate.tests.Example.steps.ConditionsOfUse;
+import org.testng.annotations.Test;
+import com.bayer.common.Navigate;
+import com.bayer.common.TimedNavigate;
+import com.bayer.test.AbstractTest;
+import com.bayer.test.device.DeviceContainer;
+import com.bayer.test.step.factory.Step;
+import org.springframework.ejb.access.SimpleRemoteStatelessSessionProxyFactoryBean;
+import com.bayer.common.utility.LinkValidator;
+import com.bayer.common.utility.StructureValidator;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.javascript.host.URL;
+import com.gargoylesoftware.htmlunit.javascript.host.dom.Text;
+
+public class ExamplePage extends AbstractTest {
+	
+public String url = "https://www.amazon.com/";
+    
+	public String getUrl(){ 
+		return url;
+	}
+    
+    @TestDescriptor( testName="Example Navigation Test" )
+    @Test ( dataProvider = "deviceList", enabled=true)
+    public void navigateTest( DeviceContainer dC ) {
+        executeSteps( new Step[] { new Navigate(url), new ExampleStep() } );
+    }
+    
+    @TestDescriptor( testName="Example Timed Navigation Test" )
+    @Test ( dataProvider = "deviceList", enabled=false)
+    public void timedNavigateTest( DeviceContainer dC ) {
+        executeSteps( new Step[] { new TimedNavigate(url, 6000) } );
+    }
+     
+}
+
+// push test 5
