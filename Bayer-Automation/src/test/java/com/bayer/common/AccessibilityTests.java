4fef959951a7898a82e27ffb38c9a5ed3ab66e84
Colin <colin.ebneth@bayer.com>
2018-03-15 09:31:46 -0400

    Added GDPR to Jenkins

--- Bayer-Automation/src/test/java/com/bayer/common/AccessibilityTests.java
+++ Bayer-Automation/src/test/java/com/bayer/common/AccessibilityTests.java
@@ -1,11 +1,19 @@
 package com.bayer.common;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.testng.annotations.Test;
 
 import com.bayer.common.utility.LinkValidator;
 import com.bayer.test.AbstractTest;
+import com.bayer.test.AbstractTest.TestDescriptor;
 import com.bayer.test.device.DeviceContainer;
 import com.bayer.test.step.factory.Step;
+import com.bayer.corp.Regions.Europe.GDPR.*;
+import com.bayer.corp.Regions.Europe.GDPR.tests.Privacy.steps.PrivacyPolicy;
 
 public class AccessibilityTests extends AbstractTest
 {
@@ -18,7 +26,6 @@ public class AccessibilityTests extends AbstractTest
         boolean checkPoint = true;
         executeSteps( new Step[] { new Navigate( baseUrl ), new LinkValidator( baseUrl, -1 ), new Accessibility( baseUrl) } );
     }
-	
     @TestDescriptor ( testName = "Pet Basics General Testing ")
     @Test ( dataProvider = "deviceList", enabled = false, groups = "ci" )
     public void petBasicAccessibility( DeviceContainer dC )
@@ -67,7 +74,77 @@ public class AccessibilityTests extends AbstractTest
         executeSteps( new Step[] { new Navigate( baseUrl ), new LinkValidator( baseUrl, -1 ), new Accessibility( baseUrl) } );
     }
    
-    
+    	private String url = "https://www.betaferon.de/";
+		private String miraID = null;
+		private String countryCode = "DE";
+		private int parentRowNum = 1;
+		private static final String FILE_NAME = "src/test/java/com/bayer/corp/Regions/Germany/GDPR/config/DataFiles/urlListBatch4.xlsx";	    
+			public String getUrl(){ 
+				return url;
+			}
+			public int getRowNum(){ 
+				return parentRowNum;
+			}
+			public void setRowNum(int newNum){ 
+				this.parentRowNum = newNum;
+			}
+			public void setUrl(String newUrl){ 
+				url = newUrl;
+			}
+			@TestDescriptor( testName="GDPR Privacy Policy Validation" )
+  		    @Test ( dataProvider = "deviceList", enabled=false, groups ="ci")
+  		    public void privacyPolicyTest( DeviceContainer dC ) {
+				String [] urlLine = new String[130];
+				int numEntries = 0;
+				try {
+					File file = new File("src/test/java/com/bayer/corp/Regions/Europe/GDPR/config/DataFiles/MasterURLlist.csv");
+					FileReader fileReader = new FileReader(file);
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+					StringBuffer stringBuffer = new StringBuffer();
+					String line;
+					int i = 0;
+					while ((line = bufferedReader.readLine()) != null) {
+						//System.out.println(line);
+						urlLine[i] = line;
+						
+						numEntries++;
+						i++; }
+					fileReader.close();
+					//System.out.println("Contents of file:");
+					//System.out.println(stringBuffer.toString());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				System.out.println("For loop");
+				
+				for(int c = 0; c < numEntries; c ++) {
+					String line = urlLine[c];
+					String[] parts = line.split(",");
+					String urlName = parts[0]; // 004
+					countryCode = parts[1];
+					miraID = parts[2];
+					url = "http://"+urlName;
+					System.out.println(urlName);
+					System.out.println(countryCode);
+					System.out.println(miraID);
+					try {
+						executeSteps( new Step[] { new Navigate(url), new PrivacyPolicy(url, parentRowNum, countryCode, miraID)});
+					} 
+					catch (Exception e) {
+						System.out.println("Unable to reach url: " +url);
+					}
+						parentRowNum++;
+					System.out.println("Parent row num is " + parentRowNum);
+				}
+			}
+			
+		    @TestDescriptor( testName="GDPR URL Test" )
+		    @Test ( dataProvider = "deviceList", enabled=true, groups = "ci")
+		    public void privacyNavigationLoop( DeviceContainer dC ) {
+		    	
+		    	executeSteps( new Step[] { new Navigate(url), new PrivacyPolicy(url, parentRowNum, countryCode, miraID) } );	
+		        
+		    }
 
     
 }
